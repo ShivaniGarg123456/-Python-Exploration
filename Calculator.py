9c2a888de53386b4c4b3be762cc85391424e4f8c
Shivani Garg <Sg2895653@gmail.com>
2025-03-01 20:16:51 +0530

    Added calculator file

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+a = float(input("Enter first number:"))
+b = float(input("Enter second number:"))
+operator = input("choose an operator from given :\n 1.Addition(+)\n 2.subtraction(-)\n 3.multiplication(*)\n 4.division(/)\n")
+if operator == "+":
+    result = a+b
+    print("Result:",result)
+elif operator == "-":
+    result = a-b
+    print("Result:",result)
+elif operator == "*":
+    result = a*b
+    print("Result:",result)
+elif operator == "/":
+    if b != 0:
+        result = a/b
+        print("Result:",result)
+    else :
+        print("Division not possible,Try again!!!")
+else :
+    print("invalid operator!!! please use any of given (+,-,*,/)")
